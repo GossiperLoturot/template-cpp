2c919e46b83367ec99c75074abf444b1d742358a
GossiperLoturot <ts20030211@gmail.com>
2025-06-14 21:51:04 +0900

    initial commit

---
+++ src/template.cpp
@@ -0,0 +1,3 @@
+#include <template.hpp>
+
+int add(int a, int b) { return a + b; }
---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <template.hpp>
+
+TEST(test, add_function) {
+  EXPECT_EQ(add(2, 3), 5);
+  EXPECT_EQ(add(-1, 1), 0);
+  EXPECT_EQ(add(0, 0), 0);
+  EXPECT_EQ(add(-5, -5), -10);
+}
